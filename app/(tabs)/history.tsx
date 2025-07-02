3e3ac97ee5e7b6ccedb45284f63f2a4602df5edb
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-07-02 13:12:20 +0530

    refactor(history): update transactions with FlatList

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -2,13 +2,15 @@ import CustomView from '@/components/CustomView';
 import HistoryCard from '@/components/HistoryCard';
 import { transactions } from '@/sample-data';
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 
 const History = () => {
     return (
         <CustomView>
-            <ScrollView>
-                {transactions.map((item, index) => (
+            <FlatList
+                data={transactions}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item, index }) => (
                     <HistoryCard
                         key={index}
                         date={item.date}
@@ -16,8 +18,8 @@ const History = () => {
                         amount={item.amount}
                         description={item.description}
                     />
-                ))}
-            </ScrollView>
+                )}
+            />
         </CustomView>
     );
 };
