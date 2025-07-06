f2227d815f3aa8ba4b2d077e914e1282f223de90
viswanadh7 <mudunuruviswanadh@gmail.com>
2025-07-06 09:59:23 +0530

    feat(context): implement user details management with secure storage

--- context/index.tsx
+++ context/index.tsx
@@ -1,18 +1,68 @@
 import { darkTheme, lightTheme, ThemeType } from '@/styles/color';
-import { createContext, ReactNode } from 'react';
+import { UserDetailsType } from '@/types/commonTypes';
+import { useRouter } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { useColorScheme } from 'react-native';
 
 type TContext = {
     theme: ThemeType;
+    userDetails?: UserDetailsType;
+    setUserDetails: (userDetails: UserDetailsType) => void;
+
+    saveUserDetails: (userDetails: UserDetailsType) => Promise<void>;
 };
 
 export const Context = createContext<TContext>({} as TContext);
 
 const ContextProvider = ({ children }: { children: ReactNode }) => {
+    const router = useRouter();
     const colorScheme = useColorScheme();
     const theme = colorScheme === 'dark' ? darkTheme : lightTheme;
 
-    return <Context.Provider value={{ theme }}>{children}</Context.Provider>;
+    const [userDetails, setUserDetails] = useState<UserDetailsType>();
+
+    useEffect(() => {
+        fetchUserDetails();
+    }, []);
+
+    // Fetch user details from secure storage
+    // This will be called when the app starts to check if user is already logged in
+    const fetchUserDetails = async () => {
+        try {
+            const userDetailsString = await SecureStore.getItemAsync(
+                'userDetails'
+            );
+            if (userDetailsString) {
+                setUserDetails(JSON.parse(userDetailsString));
+            } else clearUserDetails();
+        } catch (error) {
+            console.error('Error fetching user details:', error);
+        }
+    };
+
+    // Save user details to secure storage
+    const saveUserDetails = async (userDetails: UserDetailsType) => {
+        setUserDetails(userDetails);
+        await SecureStore.setItemAsync(
+            'userDetails',
+            JSON.stringify(userDetails)
+        );
+    };
+
+    // Clear user details from secure storage
+    const clearUserDetails = async () => {
+        await SecureStore.deleteItemAsync('userDetails');
+        router.replace('/signin'); // Redirect to sign-in page
+    };
+
+    return (
+        <Context.Provider
+            value={{ theme, userDetails, setUserDetails, saveUserDetails }}
+        >
+            {children}
+        </Context.Provider>
+    );
 };
 
 export default ContextProvider;
---
+++ types/commonTypes.ts
@@ -0,0 +1,5 @@
+export type UserDetailsType = {
+    username: string;
+    name: string;
+    password?: string;
+};
